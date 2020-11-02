937d18ceb4bf818c1b95f2478d07a0c4d9f2dc17
Jobin Babu Ayathil <jobinb6444@outlook.com>
2020-11-02 15:11:33 +0530

    Add files via upload

---
+++ src/renderer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./sass/style.scss";
+
+import Editor from "./editor";
+
+ReactDOM.render(<Editor />, document.getElementById("root"));
